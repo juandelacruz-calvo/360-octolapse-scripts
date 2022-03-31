17d450f9338913aa4ac6cdf2b44d9fc3f7113169
Juan Calvo <juandelacruz.calvo@gmail.com>
2022-03-31 23:08:37 +0200

    Fix initial delay

--- pre_print.py
+++ pre_print.py
@@ -9,10 +9,10 @@ GPIO.setmode(GPIO.BCM)
 
 
 def pre_print():
-    switch.enable_switch_hook()
-    is_homed = switch.is_camera_in_home()
-
-    if is_homed:
+    # switch.enable_switch_hook()
+    # is_homed = switch.is_camera_in_home()
+    is_homed = False
+    if not is_homed:
         try:
             motor.motor_go(True, int(motor.STEPS_PER_LOOP / 2))
         except StopMotorInterrupt:
