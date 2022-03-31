fb6a8cf1eeb8fc72b29c5f5602eb652f172ad434
Juan Calvo <juandelacruz.calvo@gmail.com>
2022-03-31 23:31:14 +0200

    ????

--- switch/switch.py
+++ switch/switch.py
@@ -17,7 +17,7 @@ def button_pressed_callback(channel):
 def enable_switch_hook():
     try:
         GPIO.add_event_detect(SWITCH_PIN, GPIO.FALLING,
-                              callback=button_pressed_callback, bouncetime=50)
+                              callback=button_pressed_callback, bouncetime=10)
     except RuntimeError as err:
         print("Error enabling switch: %s" % err)
 
