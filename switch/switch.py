ac2203eb1e7c512bab53e33b074a6a922e8d0a72
Juan Calvo <juandelacruz.calvo@gmail.com>
2022-03-31 23:05:06 +0200

    Fix initial delay

--- switch/switch.py
+++ switch/switch.py
@@ -9,10 +9,14 @@ SWITCH_PIN = 27
 GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
+def button_pressed_callback(channel):
+    motor.stop_motor()
+
+
 def enable_switch_hook():
     try:
         GPIO.add_event_detect(SWITCH_PIN, GPIO.FALLING,
-                              callback=button_pressed_callback, bouncetime=100)
+                              callback=button_pressed_callback, bouncetime=50)
     except RuntimeError as err:
         print("Error enabling switch: %s" % err)
 
@@ -22,9 +26,5 @@ def disable_switch_hook():
     time.sleep(0.5)
 
 
-def button_pressed_callback(channel):
-    motor.stop_motor()
-
-
 def is_camera_in_home() -> bool:
-    return not GPIO.input(SWITCH_PIN)
+    return GPIO.input(SWITCH_PIN)
