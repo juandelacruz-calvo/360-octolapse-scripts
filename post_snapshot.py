17d226d72f974e66ba18e85b096fa0c634b48b07
Juan Calvo <juandelacruz.calvo@gmail.com>
2022-03-31 21:20:04 +0200

    Implement switch

--- post_snapshot.py
+++ post_snapshot.py
@@ -1,9 +1,8 @@
-import sys
-
 import RPi.GPIO as GPIO
+import sys
+from RpiMotorLib.RpiMotorLib import StopMotorInterrupt
 
 import motor.motor as motor
-from RpiMotorLib.RpiMotorLib import StopMotorInterrupt
 
 SWITCH_PIN = 27
 
@@ -11,19 +10,12 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-global SWITCH_ON
-
 
 def button_pressed_callback(channel):
-    global SWITCH_ON
-    if not SWITCH_ON:
-        motor.stop_motor()
-    SWITCH_ON = True
+    motor.stop_motor()
 
 
 def post_snapshot(snapshot_number: int):
-    global SWITCH_ON
-    SWITCH_ON = False
     GPIO.add_event_detect(SWITCH_PIN, GPIO.FALLING,
                           callback=button_pressed_callback, bouncetime=50)
 
@@ -38,6 +30,7 @@ def post_snapshot(snapshot_number: int):
             raise SystemError
         finally:
             motor.disable_stepper()
+            GPIO.remove_event_detect(SWITCH_PIN)
 
 
 if __name__ == "__main__":
