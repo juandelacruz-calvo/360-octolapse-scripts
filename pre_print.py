54bdd4750d39d9ccbf865c448327f19c8d948225
Juan Calvo <juandelacruz.calvo@gmail.com>
2022-04-06 17:55:18 +0200

    Decrease preprint length

--- pre_print.py
+++ pre_print.py
@@ -7,21 +7,23 @@ from switch import switch
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+STEPS_PER_SIDE = motor.STEPS_PER_LOOP / 2 + motor.STEPS_PER_LOOP / 4
+
 
 def pre_print(enable_hook: bool = True):
     if enable_hook:
         switch.enable_switch_hook()
-        
+
     is_homed = switch.is_camera_in_home()
     if not is_homed:
         try:
             try:
-                motor.motor_go(True, int(motor.STEPS_PER_LOOP))
+                motor.motor_go(True, int(STEPS_PER_SIDE))
             except StopMotorInterrupt:
                 is_homed = True
             if not is_homed:
                 try:
-                    motor.motor_go(False, int(motor.STEPS_PER_LOOP))
+                    motor.motor_go(False, int(STEPS_PER_SIDE))
                 except StopMotorInterrupt:
                     pass
                 else:
